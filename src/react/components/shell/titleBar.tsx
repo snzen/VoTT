5ca193dcf4c8e4e028ac57ee73ced669ab67e1f3
asenrt <asenrt@users.noreply.github.com>
2020-06-09 19:01:02 +0300

    theme

--- src/react/components/shell/titleBar.tsx
+++ src/react/components/shell/titleBar.tsx
@@ -82,7 +82,7 @@ export class TitleBar extends React.Component<ITitleBarProps, ITitleBarState> {
                         </Menu>
                     </div>
                 }
-                <div className="title-bar-main">{this.props.title || "Welcome"} - VoTT</div>
+                <div className="title-bar-main">{this.props.title || "Welcome"} - Rhythm VoTT</div>
                 <div className="title-bar-controls">
                     {this.props.children}
                     {this.state.platform === PlatformType.Windows &&
